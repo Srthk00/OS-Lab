fcdcd523455046489d2912b8f5fc1fe46126f2bf
Sarthak Kothiyal <sarthakkothiyal83@gmail.com>
2025-12-12 14:09:07 +0000

    new

---
+++ first_fit_new.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int n;
+    printf("Enter RAM size: ");
+    scanf("%d",&n);
+    int *ram=(int *)malloc(n*sizeof(int));
+    for(int i=0;i<n;i++){
+        scanf("%d",&ram[i]);
+    }
+    int p;
+    printf("Enter number of processes: ");
+    scanf("%d",&p);
+    int *pro=(int *)malloc(p*sizeof(int));
+    for(int i=0;i<p;i++){
+        scanf("%d",&pro[i]);
+    }
+    int *filled=(int *)malloc(n*sizeof(int));
+    for(int i=0;i<n;i++){filled[i]=-1;}
+    for(int i=0;i<p;i++){
+        for(int j=0;j<n;j++){
+            if(ram[j]>=pro[i] && filled[j]==-1){
+                filled[j]=i;
+                break;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(filled[i]==-1){
+            printf("Block %d is empty\n",i);
+        }
+        else{
+            printf("Block %d(%d) is occupied by: %d(%d)\n",i,ram[i],filled[i],pro[filled[i]]);
+        }
+    }
+    return 0;
+}
